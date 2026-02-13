4670aaf2dc3d45801d9bee66bdcec7f9e6f742ef
Bzden <158495797+Bzden4ik@users.noreply.github.com>
2026-02-14 00:55:29 +0200

    Update

--- frontend/src/components/DecodeText.js
+++ frontend/src/components/DecodeText.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './DecodeText.css';
 
 function DecodeText({ text, className = '', delay = 0 }) {
